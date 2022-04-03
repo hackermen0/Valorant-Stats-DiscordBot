3f8c94165ed2ab545a7952ec01f9e0a81bbb5c92
Upaayan <upaayan@gmail.com>
2022-04-03 11:07:48 +0530

    Updating stats.py Fixing local variable bug with filtered_headers

--- Cogs/recent.py
+++ Cogs/recent.py
@@ -71,8 +71,8 @@ async def get_match(ctx, user_id : int, match_filter):
             return data
 
         else:
-            headers = {'filter' : match_filter, 'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.52'}
-            r = get(link, headers)
+            filtered_headers = {'filter' : match_filter, 'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.52'}
+            r = get(link, filtered_headers)
             data = r.json()
             print(r.status_code,data)
             return data
