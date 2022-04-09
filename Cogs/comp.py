7082c463b991c6fbcd542de3d4d6affe044e40e3
Upaayan <upaayan@gmail.com>
2022-04-09 18:32:40 +0530

    ADding comment

--- Cogs/comp.py
+++ Cogs/comp.py
@@ -9,6 +9,8 @@ import requests
 import os
 
 
+#Created by hackerman
+
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.52'}
